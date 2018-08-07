912e4be61b6d82ade0d95398b0c2eb3a8ce39ce3
Sarita Raghunath <sarita.raghunath@gmail.com>
2018-08-07 14:55:30 +0000

    automatically backed up by learn

--- crazy_string.rb
+++ crazy_string.rb
@@ -4,5 +4,5 @@ def crazy_strings(string_one, string_two)
   string_one = string_one.upcase.reverse
   string_two = string_two.swapcase.gsub("s" , "z")
   return "#{string_one} #{string_two"  
-
+end
 
