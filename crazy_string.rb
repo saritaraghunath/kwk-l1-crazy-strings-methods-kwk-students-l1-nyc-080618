ebb4f29b0c843022d46280b329a27f9bbb823ef2
Sarita Raghunath <sarita.raghunath@gmail.com>
2018-08-07 14:51:21 +0000

    automatically backed up by learn

--- crazy_string.rb
+++ crazy_string.rb
@@ -2,6 +2,7 @@
 
 def crazy_strings(string1,string2)
 string1 = string1.reverse.upcase
-string2 = string2.gsub("s", "z").swapcase
+string2 = string2.swapcase.gsub("S", "z").
   return "#{string1} #{string2}"
 end
+crazy_strings("Hello", "Friends")
